411682c5f7dddbbc2f747ff61b9e3f7751164c42
LiberoBarte <Bookchamber00@gmail.com>
2023-10-26 23:00:42 +0900

    play system
    맵 돌리기 쿨타임 적용

--- Assets/Scripts/StageController.cs
+++ Assets/Scripts/StageController.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class StageController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject player;
     public float rotationSpeed = 5.0f;
     private float targetRotationX = 0.0f;
     private bool isRotating = false;
 
-    void Start()
+    private float rotateCoolTime = 3f;
+    [SerializeField]
+    private bool canRotate;
+
+    private void Awake()
     {
         // 게임 시작 시 초기 회전 설정
         targetRotationX = 0.0f;
+        canRotate = true;
     }
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P) && !isRotating)
+        if(isRotating)
+            player.GetComponent<Rigidbody>().useGravity = false;
+        else
+            player.GetComponent<Rigidbody>().useGravity = true;
+
+        if (Input.GetKeyDown(KeyCode.P) && !isRotating & canRotate)
         {
             // P를 누를 때마다 90도씩 회전
             targetRotationX += 90.0f;
             isRotating = true;
             RotateStage();
         }
-        else if (Input.GetKeyDown(KeyCode.O) && !isRotating)
+        else if (Input.GetKeyDown(KeyCode.O) && !isRotating & canRotate)
         {
             // O를 누를 때마다 -90도씩 회
             targetRotationX -= 90.0f;
@@ -32,13 +44,14 @@ public class StageController : MonoBehaviour
         }
     }
 
-    void RotateStage()
+    private void RotateStage()
     {
         Quaternion targetRotation = Quaternion.Euler(targetRotationX, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
         StartCoroutine(LerpRotation(targetRotation, rotationSpeed));
+        StartCoroutine(RotateCoolTime());
     }
 
-    System.Collections.IEnumerator LerpRotation(Quaternion targetRotation, float timeToRotate)
+    private IEnumerator LerpRotation(Quaternion targetRotation, float timeToRotate)
     {
         Quaternion startRotation = transform.rotation;
         float elapsedTime = 0;
@@ -52,6 +65,13 @@ public class StageController : MonoBehaviour
 
         transform.rotation = targetRotation;
         isRotating = false;
+    }
+
+    private IEnumerator RotateCoolTime() 
+    {
+        canRotate = false;
+        yield return new WaitForSeconds(rotateCoolTime);
+        canRotate = true;
     }
 }
 
